513367573836b0d5fb779b9c98bc93e03f6d4b27
legendddhgf <legendddhgf@gmail.com>
2016-12-29 20:44:20 -0800

    Added parsing for options and tags
    comments here and there as well

--- GameEngine.cpp
+++ GameEngine.cpp
@@ -1,16 +1,23 @@
 #include "GameEngine.h"
+
 GameEngine::GameEngine (FILE *fp) {
     list_rooms.clear(); // are these done automatically?
     list_description.clear();
     map_opts.clear();
     map_tags.clear();
 
-    char *buf = (char *) calloc(1024, sizeof(char));
-    char *modstr = (char *) calloc(1024, sizeof(char)); // modstr can be at most str size of buf + 1
+    char *buf = (char *) calloc(1024, sizeof(char)); // this is simply a temporary string which gets trimmed to modstr
+    char *modstr = (char *) calloc(1024, sizeof(char)); // THIS IS THE LINE WE ARE READING!!!
+    int linecount = 0;
     while (fgets(buf, 1023, fp)) {
+        linecount++;
         stringTrim(buf, "\n\r ", modstr); // trim newline, carriage return, and space from front and back
         if (strlen(modstr) == 0) { // skip empty lines
             continue;
+        }
+        if (strlen(modstr) == 1) {
+            fprintf(stderr, "line %d: No parameters specified\n", linecount);
+            exit(1);
         }
         switch (modstr[0]) {
             case 'r': // room
@@ -18,7 +25,10 @@ GameEngine::GameEngine (FILE *fp) {
                     fprintf(stderr, "%s: Room has no description\n", list_rooms.back().c_str());
                     exit(1);
                 }
-                // TODO: check that all options have atleast one tag
+                if (map_opts.size() != map_tags.size()) {
+                    fprintf(stderr, "line %d: There must be one tag for every option\n", linecount);
+                    exit(1);
+                }
                 list_rooms.push_back(string(modstr + 2)); // skip the 'r' and the ' '
                 break;
             case 'd': // description
@@ -39,13 +49,45 @@ GameEngine::GameEngine (FILE *fp) {
                 }
                 break;
             case 'o':
-                // string for indexing into map is back of room list
-                //if (!inserted into opts) {
-                    //insert empty vector
-                //}
-                // append to the vector
+                {
+                if (list_rooms.size() == 0) {
+                    fprintf(stderr, "line %d: Must have at least one room to specify options\n", linecount);
+                    exit(1);
+                }
+                if (map_opts.size() != map_tags.size()) {
+                    fprintf(stderr, "line %d: Expected another tag first\n", linecount);
+                    exit(1);
+                }
+                // if there are no transitions from the current room, insert
+                // a blank one
+                if (map_opts.find(list_rooms.back()) == map_opts.end())
+                    map_opts.insert(pair<string, vector<string>>(list_rooms.back(), {}));
+                // finally, append to the appropriate vector
+                auto list_opts = map_opts.find(list_rooms.back())->second;
+                //list_opts.push_back(string(modstr + 2));
                 break;
+                }
             case 't':
+                {
+                if (list_rooms.size() == 0) {
+                    fprintf(stderr, "line %d: Must have at least one room to specify tags\n", linecount);
+                    exit(1);
+                }
+                if (map_opts.size() == map_tags.size()) {
+                    fprintf(stderr, "line %d: Expected another option first\n", linecount);
+                    exit(1);
+                }
+                // if there are no transitions from the current room, insert
+                // a blank one
+                if (map_tags.find(list_rooms.back()) == map_tags.end())
+                    map_tags.insert(pair<string, vector<string>>(list_rooms.back(), {}));
+                // finally, append to the appropriate vector
+                auto list_tags = map_tags.find(list_rooms.back())->second;
+                //list_tags.push_back(string(modstr + 2));
+                break;
+                }
+            case 'g':
+                // TODO: PLZ
                 break;
             default:
                 fprintf(stderr, "%c: No Corresponding field\n", modstr[0]);
--- GameEngine.h
+++ GameEngine.h
@@ -7,8 +7,12 @@ typedef struct GameEngine {
 
     vector <string> list_rooms;
     vector <string> list_description;
-    unordered_map <string, vector <string>> map_opts;
-    unordered_map <string, vector <string>> map_tags;
+    // for the following three, the key is the current room
+    unordered_map <string, vector <string>> map_opts; // list of room/group names
+    unordered_map <string, vector <string>> map_tags; // list of descriptions for transitions
+    // each pair is the group name with a list of rooms and corresponding
+    // priorities (to assign possibility of certain rooms being chosen)
+    unordered_map <string, vector <pair<string, int>>> map_groups;
 
     GameEngine(FILE *);
 
